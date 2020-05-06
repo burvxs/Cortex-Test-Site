468b289003c2327d5b340c26f27a756b2902ea06
Ben Naumoff <benny.purvis@hotmail.com>
2020-05-06 18:49:04 +1000

    Added EMAIL_BACKEND value

--- cortexwebsite/settings/production.py
+++ cortexwebsite/settings/production.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "slurpehediits@gmail.com"
 EMAIL_HOST_PASSWORD = "DerpDerp"
