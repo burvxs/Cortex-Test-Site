3f0a5fba2e95d1dd3ea5e4ecdaf88458556aeec4
Ben Naumoff <benny.purvis@hotmail.com>
2020-05-06 22:50:46 +1000

    Added defaults to environ.get

--- cortexwebsite/aws/conf.py
+++ cortexwebsite/aws/conf.py
@@ -1,10 +1,11 @@
 import datetime
 import os
+import aws.hide_aws_keys as hidden_keys
 
 AWS_GROUP_NAME = "Cortex-TestSite-Group"
 AWS_USERNAME = "cortex-test-site-user"
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", getattr(hidden_keys, "AWS_ACCESS_KEY_ID", None))
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", getattr(hidden_keys, "AWS_SECRET_ACCESS_KEY", None))
 
 AWS_FILE_EXPIRE = 200
 AWS_PRELOAD_METADATA = True 
