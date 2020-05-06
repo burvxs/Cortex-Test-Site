0818b0117f3c518ec9c7e97be62ae78cccc99112
Ben Naumoff <benny.purvis@hotmail.com>
2020-05-06 18:17:10 +1000

    Updated EMAIL_HOST_PASSWORD from settings

--- cortexwebsite/settings/production.py
+++ cortexwebsite/settings/production.py
@@ -22,8 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
