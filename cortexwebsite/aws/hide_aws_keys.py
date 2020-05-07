18fe9c3a8c4654f90ff3af39bf3ebe40943581ff
Ben Naumoff <benny.purvis@hotmail.com>
2020-05-07 13:53:53 +1000

    Put file read into try block

--- cortexwebsite/aws/hide_aws_keys.py
+++ cortexwebsite/aws/hide_aws_keys.py
@@ -1,6 +1,10 @@
-key_file = open("cortexwebsite/aws/keys.txt", "r")
-lines = key_file.read().split("\n")
-access_key = lines[0]
-secret_key = lines[1]
-AWS_ACCESS_KEY_ID = access_key
-AWS_SECRET_ACCESS_KEY = secret_key
+try:
+    key_file = open("cortexwebsite/aws/keys.txt", "r")
+    lines = key_file.read().split("\n")
+    access_key = lines[0]
+    secret_key = lines[1]
+except:
+    pass
+if access_key and secret_key != None:
+    AWS_ACCESS_KEY_ID = access_key
+    AWS_SECRET_ACCESS_KEY = secret_key
